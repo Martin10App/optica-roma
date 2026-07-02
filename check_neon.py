00ea810a6037e3e341ec231f3ef809bbafaabedc
Martin <opticaromalaspiedras@hotmail.com>
2026-07-02 13:22:35 -0300

    chore: remove scripts with hardcoded credentials from repo

--- check_neon.py
+++
@@ -1,8 +0,0 @@
-import psycopg
-try:
-    conn = psycopg.connect('postgresql://neondb_owner:npg_gTbrcCaG6RQ0@ep-weathered-flower-ac86t77j.sa-east-1.aws.neon.tech/neondb?sslmode=require', connect_timeout=10)
-    cur = conn.cursor()
-    cur.execute("SELECT COUNT(*) FROM armazones_publico")
-    print("Neon rows:", cur.fetchone()[0])
-except Exception as e:
-    print("Error:", e)
